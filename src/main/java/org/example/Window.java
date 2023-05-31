57f7265b176102c8a5fc8bbdb0879a1922a7878a
Shimon <shimondj@gmail.com>
2023-05-31 17:11:59 +0300

    Changes by Noam

--- src/main/java/org/example/Window.java
+++ src/main/java/org/example/Window.java
@@ -44,18 +44,17 @@ public class Window extends JFrame {//
     }
 
     public void openConversation(String phoneNumber, String textMessage) {
-        search = chromeDriver.findElement(By.xpath("//*[@id=\"side\"]/div[1]/div/div/div[2]/div/div[1]/p"));
-        search.sendKeys(phoneNumber);
-        search.sendKeys(Keys.ENTER);
+
+        this.chromeDriver.get(Constants.WHATSAPP_CONV_LINK + phoneNumber);
         try {
-            Thread.sleep(100);
+            Thread.sleep(Constants.OPEN_CONV_THREAD_SLEEP_IN_MILLIS);
         } catch (Exception f) {
         }
 
         WebElement sendMessage1 = null;
         WebElement sendMessage2 = null;
         try {
-            Thread.sleep(100);
+            Thread.sleep(Constants.TENTH_OF_SECOND_IN_MILLIS);
             sendMessage1 = chromeDriver.findElement(By.xpath("//*[@id=\"main\"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p"));
         } catch (Exception e) {
 
